0771fb96347afffe5c4d111083a201eca9d4b865
BJNSTNKVC <dev.bojan.stankovic@gmail.com>
2024-02-12 22:06:06 +0100

    #4 Moved Eloquent builder construct to its own abstract class. Extended generated builder class with the aforementioned class. Made corrections to builder stub file.

--- src/Concerns/BuildsDynamicWhereQueries.php
+++ src/Concerns/BuildsDynamicWhereQueries.php
@@ -7,202 +7,208 @@ use Illuminate\Support\Str;
 
 trait BuildsDynamicWhereQueries
 {
-    /**
-     * Dynamically handle calls into the query instance.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return mixed
-     */
-    public function __call($method, $parameters)
-    {
-        if ($this->getDynamicMethod($method) === 'where') {
-            return $this->dynamicWhere($method, $parameters);
-        }
+	/**
+	 * Dynamically handle calls into the query instance.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return mixed
+	 */
+	public function __call($method, $parameters)
+	{
+		if ($this->getDynamicMethod($method) === 'where') {
+			return $this->dynamicWhere($method, $parameters);
+		}
 
-        if ($this->getDynamicMethod($method) === 'whereIn') {
-            return $this->dynamicWhereIn($method, $parameters);
-        }
+		if ($this->getDynamicMethod($method) === 'whereIn') {
+			return $this->dynamicWhereIn($method, $parameters);
+		}
 
-        if ($this->getDynamicMethod($method) === 'whereNotIn') {
-            return $this->dynamicWhereNotIn($method, $parameters);
-        }
+		if ($this->getDynamicMethod($method) === 'whereNotIn') {
+			return $this->dynamicWhereNotIn($method, $parameters);
+		}
 
-        if ($this->getDynamicMethod($method) === 'orWhere') {
-            return $this->dynamicOrWhere($method, $parameters);
-        }
+		if ($this->getDynamicMethod($method) === 'orWhere') {
+			return $this->dynamicOrWhere($method, $parameters);
+		}
 
-        if ($this->getDynamicMethod($method) === 'orWhereIn') {
-            return $this->dynamicOrWhereIn($method, $parameters);
-        }
+		if ($this->getDynamicMethod($method) === 'orWhereIn') {
+			return $this->dynamicOrWhereIn($method, $parameters);
+		}
 
-        if ($this->getDynamicMethod($method) === 'orWhereNotIn') {
-            return $this->dynamicOrWhereNotIn($method, $parameters);
-        }
+		if ($this->getDynamicMethod($method) === 'orWhereNotIn') {
+			return $this->dynamicOrWhereNotIn($method, $parameters);
+		}
 
-        return parent::__call($method, $parameters);
-    }
+		return parent::__call($method, $parameters);
+	}
 
-    /**
-     * Determine if the given method is a dynamic where method.
-     *
-     * @param string $method
-     *
-     * @return string
-     */
-    protected function getDynamicMethod(string $method): string
-    {
-        if ($method !== 'where' && str_starts_with($method, 'where') && !str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
-            return 'where';
-        }
+	/**
+	 * Handles dynamic "or where" clauses to the query.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return static
+	 */
+	protected function dynamicWhere(string $method, array $parameters): static
+	{
+		$column = Str::of($method)
+			->substr(5)
+			->ucsplit()
+			->map(fn(string $word) => strtolower($word))
+			->implode('_');
 
-        if ($method !== 'whereIn' && str_starts_with($method, 'where') && str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
-            return 'whereIn';
-        }
+		$operator = count($parameters) === 2 ? reset($parameters) : '=';
+		$value    = count($parameters) === 1 ? reset($parameters) : end($parameters);
 
-        if ($method !== 'whereNotIn' && str_starts_with($method, 'where') && str_ends_with($method, 'NotIn')) {
-            return 'whereNotIn';
-        }
+		return $this->where($column, $operator, $value);
+	}
 
-        if ($method !== 'orWhere' && str_starts_with($method, 'orWhere') && !str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
-            return 'orWhere';
-        }
+	/**
+	 * Handles dynamic "where in" clauses to the query.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return static
+	 */
+	protected function dynamicWhereIn(string $method, array $parameters): static
+	{
+		$column = Str::of($method)
+			->substr(5)
+			->substr(0, -2)
+			->ucsplit()
+			->map(fn(string $word) => strtolower($word))
+			->implode('_');
 
-        if ($method !== 'orWhereIn' && str_starts_with($method, 'orWhere') && str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
-            return 'orWhereIn';
-        }
+		$values = Arr::flatten($parameters);
 
-        if ($method !== 'orWhereNotIn' && str_starts_with($method, 'orWhere') && str_ends_with($method, 'NotIn')) {
-            return 'orWhereNotIn';
-        }
+		return $this->whereIn($column, $values);
+	}
 
-        return $method;
-    }
+	/**
+	 * Handles dynamic "where not in" clauses to the query.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return static
+	 */
+	protected function dynamicWhereNotIn(string $method, array $parameters): static
+	{
+		$column = Str::of($method)
+			->substr(5)
+			->substr(0, -5)
+			->ucsplit()
+			->map(fn(string $word) => strtolower($word))
+			->implode('_');
 
-    /**
-     * Handles dynamic "or where" clauses to the query.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return static
-     */
-    public function dynamicWhere(string $method, array $parameters): static
-    {
-        $column = Str::of($method)
-            ->substr(5)
-            ->slug()
-            ->toString();
+		$values = Arr::flatten($parameters);
 
-        $operator = count($parameters) === 2 ? reset($parameters) : '=';
-        $value    = count($parameters) === 1 ? reset($parameters) : end($parameters);
+		return $this->whereNotIn($column, $values);
+	}
 
-        return $this->where($column, $operator, $value);
-    }
+	/**
+	 * Handles dynamic "or where" clauses to the query.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return static
+	 */
+	protected function dynamicOrWhere(string $method, array $parameters): static
+	{
+		$column = Str::of($method)
+			->substr(7)
+			->ucsplit()
+			->map(fn(string $word) => strtolower($word))
+			->implode('_');
 
-    /**
-     * Handles dynamic "where in" clauses to the query.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return static
-     */
-    public function dynamicWhereIn(string $method, array $parameters): static
-    {
-        $column = Str::of($method)
-            ->substr(5)
-            ->substr(0, -2)
-            ->slug()
-            ->toString();
+		$operator = count($parameters) === 2 ? reset($parameters) : '=';
+		$value    = count($parameters) === 1 ? reset($parameters) : end($parameters);
 
-        $values = Arr::flatten($parameters);
+		return $this->orWhere($column, $operator, $value);
+	}
 
-        return $this->whereIn($column, $values);
-    }
+	/**
+	 * Handles dynamic "or where in" clauses to the query.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return static
+	 */
+	protected function dynamicOrWhereIn(string $method, array $parameters): static
+	{
+		$column = Str::of($method)
+			->substr(7)
+			->substr(0, -2)
+			->ucsplit()
+			->map(fn(string $word) => strtolower($word))
+			->implode('_');
 
-    /**
-     * Handles dynamic "where not in" clauses to the query.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return static
-     */
-    public function dynamicWhereNotIn(string $method, array $parameters): static
-    {
-        $column = Str::of($method)
-            ->substr(5)
-            ->substr(0, -5)
-            ->slug()
-            ->toString();
+		$values = Arr::flatten($parameters);
 
-        $values = Arr::flatten($parameters);
+		return $this->orWhereIn($column, $values);
+	}
 
-        return $this->whereNotIn($column, $values);
-    }
+	/**
+	 * Handles dynamic "or where not in" clauses to the query.
+	 *
+	 * @param string $method
+	 * @param array  $parameters
+	 *
+	 * @return static
+	 */
+	protected function dynamicOrWhereNotIn(string $method, array $parameters): static
+	{
+		$column = Str::of($method)
+			->substr(7)
+			->substr(0, -5)
+			->ucsplit()
+			->map(fn(string $word) => strtolower($word))
+			->implode('_');
 
-    /**
-     * Handles dynamic "or where" clauses to the query.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return static
-     */
-    public function dynamicOrWhere(string $method, array $parameters): static
-    {
-        $column = Str::of($method)
-            ->substr(7)
-            ->slug()
-            ->toString();
+		$values = Arr::flatten($parameters);
 
-        $operator = count($parameters) === 2 ? reset($parameters) : '=';
-        $value    = count($parameters) === 1 ? reset($parameters) : end($parameters);
+		return $this->orWhereNotIn($column, $values);
+	}
 
-        return $this->orWhere($column, $operator, $value);
-    }
+	/**
+	 * Retrieve the dynamic method for the builder, return false otherwise.
+	 *
+	 * @param string $method
+	 *
+	 * @return string|bool
+	 */
+	protected function getDynamicMethod(string $method): string|bool
+	{
+		if ($method !== 'where' && str_starts_with($method, 'where') && !str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
+			return 'where';
+		}
 
-    /**
-     * Handles dynamic "or where in" clauses to the query.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return static
-     */
-    public function dynamicOrWhereIn(string $method, array $parameters): static
-    {
-        $column = Str::of($method)
-            ->substr(7)
-            ->substr(0, -2)
-            ->slug()
-            ->toString();
+		if ($method !== 'whereIn' && str_starts_with($method, 'where') && str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
+			return 'whereIn';
+		}
 
-        $values = Arr::flatten($parameters);
+		if ($method !== 'whereNotIn' && str_starts_with($method, 'where') && str_ends_with($method, 'NotIn')) {
+			return 'whereNotIn';
+		}
 
-        return $this->orWhereIn($column, $values);
-    }
+		if ($method !== 'orWhere' && str_starts_with($method, 'orWhere') && !str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
+			return 'orWhere';
+		}
 
-    /**
-     * Handles dynamic "or where not in" clauses to the query.
-     *
-     * @param string $method
-     * @param array  $parameters
-     *
-     * @return static
-     */
-    public function dynamicOrWhereNotIn(string $method, array $parameters): static
-    {
-        $column = Str::of($method)
-            ->substr(7)
-            ->substr(0, -5)
-            ->slug()
-            ->toString();
+		if ($method !== 'orWhereIn' && str_starts_with($method, 'orWhere') && str_ends_with($method, 'In') && !str_ends_with($method, 'NotIn')) {
+			return 'orWhereIn';
+		}
 
-        $values = Arr::flatten($parameters);
+		if ($method !== 'orWhereNotIn' && str_starts_with($method, 'orWhere') && str_ends_with($method, 'NotIn')) {
+			return 'orWhereNotIn';
+		}
 
-        return $this->orWhereNotIn($column, $values);
-    }
+		return false;
+	}
 }
