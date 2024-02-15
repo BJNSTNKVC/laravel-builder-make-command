4e1a776974816f91940933ca725057b81331706f
BJNSTNKVC <dev.bojan.stankovic@gmail.com>
2024-02-15 20:58:32 +0100

    #14 Added Clause Enum class holding possible query 'where' clauses, as well as generating builder method signatures. Simplified the BuildsDynamicWhereQueries trait __call method. Removed unnecessary Builder abstract class. Modified stub to no longer extend the aforementioned class, it now extends Eloquent builder directly.

--- src/Console/Commands/BuilderMakeCommand.php
+++ src/Console/Commands/BuilderMakeCommand.php
@@ -2,6 +2,7 @@
 
 namespace Bjnstnkvc\BuilderMakeCommand\Console\Commands;
 
+use Bjnstnkvc\BuilderMakeCommand\Enums\Clause;
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
@@ -11,190 +12,184 @@ use Illuminate\Support\Str;
 
 class BuilderMakeCommand extends GeneratorCommand implements PromptsForMissingInput
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'make:builder {name : The name of the builder} {model? : The name of the model} {--force}';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:builder {name : The name of the builder} {model? : The name of the model} {--force}';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Create a new Eloquent builder class';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Eloquent builder class';
 
-	/**
-	 * The type of class being generated.
-	 *
-	 * @var string
-	 */
-	protected $type = 'Builder';
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Builder';
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return false|void
-	 *
-	 * @throws FileNotFoundException
-	 */
-	public function handle()
-	{
-		if (parent::handle() === false && !$this->option('force')) {
-			return false;
-		}
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return false|void
+     *
+     * @throws FileNotFoundException
+     */
+    public function handle()
+    {
+        if (parent::handle() === false && !$this->option('force')) {
+            return false;
+        }
+    }
 
-	/**
-	 * Get the stub file for the generator.
-	 *
-	 * @return string
-	 */
-	protected function getStub(): string
-	{
-		return $this->resolveStubPath('/stubs/builder.stub');
-	}
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub(): string
+    {
+        return $this->resolveStubPath('/stubs/builder.stub');
+    }
 
-	/**
-	 * Resolve the fully qualified path to the stub.
-	 *
-	 * @param string $stub
-	 *
-	 * @return string
-	 */
-	protected function resolveStubPath(string $stub): string
-	{
-		return file_exists($customPath = $this->laravel->basePath(trim($stub, '/'))) ? $customPath : __DIR__ . $stub;
-	}
+    /**
+     * Resolve the fully qualified path to the stub.
+     *
+     * @param string $stub
+     *
+     * @return string
+     */
+    protected function resolveStubPath(string $stub): string
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/'))) ? $customPath : __DIR__ . $stub;
+    }
 
-	/**
-	 * Get the default namespace for the class.
-	 *
-	 * @param string $rootNamespace
-	 *
-	 * @return string
-	 */
-	protected function getDefaultNamespace($rootNamespace): string
-	{
-		return is_dir(app_path('Models')) ? "{$rootNamespace}\Models\Builders" : $rootNamespace;
-	}
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return "{$rootNamespace}\Models\Builders";
+    }
 
-	/**
-	 * Build the class with the given name.
-	 *
-	 * @param string $name
-	 *
-	 * @return string
-	 *
-	 * @throws FileNotFoundException
-	 */
-	protected function buildClass($name): string
-	{
-		$stub = $this->files->get($this->getStub());
+    /**
+     * Build the class with the given name.
+     *
+     * @param string $name
+     *
+     * @return string
+     *
+     * @throws FileNotFoundException
+     */
+    protected function buildClass($name): string
+    {
+        $stub = $this->files->get($this->getStub());
 
-		return $this->replaceNamespace($stub, $name)->replaceMethodSignatures($stub)->replaceClass($stub, $name);
-	}
+        return $this->replaceNamespace($stub, $name)->replaceMethodSignatures($stub)->replaceClass($stub, $name);
+    }
 
-	/**
-	 * Replace the method signatures for the given stub.
-	 *
-	 * @param string $stub
-	 *
-	 * @return $this
-	 */
-	protected function replaceMethodSignatures(string &$stub): static
-	{
-		$signatures = $this->getMethodSignatures();
+    /**
+     * Replace the method signatures for the given stub.
+     *
+     * @param string $stub
+     *
+     * @return $this
+     */
+    protected function replaceMethodSignatures(string &$stub): static
+    {
+        $signatures = $this->getMethodSignatures();
 
-		$stub = str_replace('DummySignature', $signatures, $stub);
+        $stub = str_replace('DummySignature', $signatures, $stub);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get the method signatures for the builder.
-	 *
-	 * @return string
-	 */
-	protected function getMethodSignatures(): string
-	{
-		$model   = $this->getModel();
-		$columns = $this->getModelColumns($model);
+    /**
+     * Get the method signatures for the builder.
+     *
+     * @return string
+     */
+    protected function getMethodSignatures(): string
+    {
+        $model   = $this->getModel();
+        $columns = $this->getModelColumns($model);
 
-		return Collection::make($columns)
-			->map(fn(string $column) => $this->setMethodSignaturesForColumn($column))
-			->implode(PHP_EOL);
-	}
+        return Collection::make($columns)
+            ->map(fn(string $column) => $this->setMethodSignaturesForColumn($column))
+            ->implode(PHP_EOL);
+    }
 
-	/**
-	 * Get the model instance from the builder name.
-	 *
-	 * @return Model
-	 */
-	protected function getModel(): Model
-	{
-		$class     = Str::replace('Builder', '', $this->argument('model') ?? $this->argument('name'));
-		$namespace = $this->laravel->getNamespace() . 'Models';
-		$model     = "$namespace\\$class";
+    /**
+     * Get the model instance from the builder name.
+     *
+     * @return Model
+     */
+    protected function getModel(): Model
+    {
+        $class     = Str::replace('Builder', '', $this->argument('model') ?? $this->argument('name'));
+        $namespace = $this->laravel->getNamespace() . 'Models';
+        $model     = "$namespace\\$class";
 
-		return new $model;
-	}
+        return new $model;
+    }
 
-	/**
-	 * Get the column listing for a given model.
-	 *
-	 * @param Model $model
-	 *
-	 * @return array
-	 */
-	protected function getModelColumns(Model $model): array
-	{
-		return $model
-			->getConnection()
-			->getSchemaBuilder()
-			->getColumnListing($model->getTable());
-	}
+    /**
+     * Get the column listing for a given model.
+     *
+     * @param Model $model
+     *
+     * @return array
+     */
+    protected function getModelColumns(Model $model): array
+    {
+        return $model
+            ->getConnection()
+            ->getSchemaBuilder()
+            ->getColumnListing($model->getTable());
+    }
 
-	/**
-	 * Set the method signatures for the builder.
-	 *
-	 * @param string $column
-	 *
-	 * @return string
-	 */
-	protected function setMethodSignaturesForColumn(string $column): string
-	{
-		$method = Str::studly($column);
+    /**
+     * Set the method signatures for the builder.
+     *
+     * @param string $column
+     *
+     * @return string
+     */
+    protected function setMethodSignaturesForColumn(string $column): string
+    {
+        $method = Str::studly($column);
 
-		return
-			' * @method static DummyClass where' . $method . '(?string $operator = null, ?string $value = null) Add a "where" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method DummyClass where' . $method . '(?string $operator = null, ?string $value = null) Add a "where" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method static DummyClass orWhere' . $method . '(?string $operator = null, ?string $value = null) Add an "or where" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method DummyClass orWhere' . $method . '(?string $operator = null, ?string $value = null) Add an "or where" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method static DummyClass where' . $method . 'In(array $values) Add a "where in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method DummyClass where' . $method . 'In(array $values) Add a "where in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method static DummyClass orWhere' . $method . 'In(array $values) Add an "or where in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method DummyClass orWhere' . $method . 'In(array $values) Add an "or where in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method static DummyClass where' . $method . 'NotIn(array $values) Add a "where not in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method DummyClass where' . $method . 'NotIn(array $values) Add a "where not in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method static DummyClass orWhere' . $method . 'NotIn(array $values) Add a "where not in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' * @method DummyClass orWhere' . $method . 'NotIn(array $values) Add a "where not in" clause on the "' . $column . '" column to the query.' . PHP_EOL .
-			' *';
-	}
+        return
+            Clause::WHERE->toMethodSignature($method, $column) .
+            Clause::OR_WHERE->toMethodSignature($method, $column) .
+            Clause::WHERE_IN->toMethodSignature($method, $column) .
+            Clause::OR_WHERE_IN->toMethodSignature($method, $column) .
+            Clause::WHERE_NOT_IN->toMethodSignature($method, $column) .
+            Clause::OR_WHERE_NOT_IN->toMethodSignature($method, $column) .
+            ' *';
+    }
 
-	/**
-	 * Prompt for missing input arguments using the returned questions.
-	 *
-	 * @return array
-	 */
-	protected function promptForMissingArgumentsUsing(): array
-	{
-		return [
-			'name' => [
-				'What should the builder be named?',
-				'E.g. PodcastBuilder',
-			],
-		];
-	}
+    /**
+     * Prompt for missing input arguments using the returned questions.
+     *
+     * @return array
+     */
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'name' => [
+                'What should the builder be named?',
+                'E.g. PodcastBuilder',
+            ],
+        ];
+    }
 }
