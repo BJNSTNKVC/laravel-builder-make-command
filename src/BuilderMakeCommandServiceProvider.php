6f38c2e005bd101b5914f8acdc6cfbfccdaff2fa
BJNSTNKVC <dev.bojan.stankovic@gmail.com>
2024-07-18 19:07:57 +0200

    #24 In case no arguments are passed, console command will now prompt a User for values. Added builder config file which can be published and customized. Builder stubs can be published and customized. Updated documentation.

--- src/BuilderMakeCommandServiceProvider.php
+++ src/BuilderMakeCommandServiceProvider.php
@@ -14,10 +14,31 @@ class BuilderMakeCommandServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/builder.php',
+            'builder'
+        );
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 BuilderMakeCommand::class,
             ]);
         }
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/config/builder.php' => config_path('builder.php'),
+        ], 'make-builder-config');
+
+        $this->publishes([
+            __DIR__ . '/Console/Commands/stubs' => base_path('stubs'),
+        ], 'make-builder-stubs');
     }
 }
